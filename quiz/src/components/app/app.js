b41bb8bcc9ff14b24d6bbd83d58007a6a2e7bfdd
DenSznk <densznk@gmail.com>
2024-03-20 21:24:19 +0400

    added quiz panel styles

--- quiz/src/components/app/app.js
+++ quiz/src/components/app/app.js
@@ -1,12 +1,15 @@
 import './app.scss'
 import  Header from '../header/header'
 import ButtonsPanel from '../buttons-panel/buttons-panel'
+import QuizPanel from '../quiz-panel/quiz-panel'
 
 function App() {
     return (
         <div className="app">
             <Header/>
+            <QuizPanel/>
             <ButtonsPanel/>
+
         </div>
     )
 }
---
+++ quiz/src/components/quiz-panel/quiz-panel.js
@@ -0,0 +1,25 @@
+import "./quiz-panel.scss"
+
+function QuizPanel() {
+    return (
+        <div className="quiz-panel">
+            <label className="number-of-questions"> Number of Questions:
+                <input type="text"/>
+            </label>
+            <label className="category"> Category:
+                <input type="text"/>
+            </label>
+            <label className="difficulties"> Difficulty:
+                <select type="text"/>
+            </label>
+            <label className="type"> Type:
+                <input type="text"/>
+            </label>
+            <label className="time"> Time:
+                <select type="text"/>
+            </label>
+        </div>
+    )
+}
+
+export default QuizPanel
