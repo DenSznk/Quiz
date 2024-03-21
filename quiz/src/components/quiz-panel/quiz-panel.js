b57a404a13825489afc4544c3c2199069ee3e1e2
DenSznk <densznk@gmail.com>
2024-03-21 13:09:45 +0400

    added select styles

--- quiz/src/components/quiz-panel/quiz-panel.js
+++ quiz/src/components/quiz-panel/quiz-panel.js
@@ -4,22 +4,47 @@ function QuizPanel() {
     return (
         <div className="quiz-panel">
           <div className="container">
-          <label className="number-of-questions"> Number of Questions:
-                <input type="text"/>
+          <label className="number-of-questions"> Number of Questions
+                <select className="select">
+                    <option value="value1">5</option>
+                    <option value="value2">6</option>
+                    <option value="value3">7</option>
+                    <option value="value1">8</option>
+                    <option value="value2">9</option>
+                    <option value="value3">10</option>
+                    <option value="value1">11</option>
+                    <option value="value2">12</option>
+                    <option value="value3">13</option>
+                    <option value="value3">14</option>
+                    <option value="value3">15</option>
+                </select>
             </label>
           </div>
-            <label className="category"> Category:
-                <input type="text"/>
+            <div className="container">
+                <label className="category"> Category
+                        <select className="select"></select>
+                </label>
+           </div>
+           <div className="container">
+                <label className="difficulties"> Difficulty
+                        <select className="select"></select>
+                </label>
+            </div>
+            <div className="container">
+                <label className="type"> Type
+                    <select className="select">
+                </select>
             </label>
-            <label className="difficulties"> Difficulty:
-                <select type="text"/>
-            </label>
-            <label className="type"> Type:
-                <input type="text"/>
-            </label>
-            <label className="time"> Time:
-                <select type="text"/>
-            </label>
+            </div>
+            <div className="container">
+                <label className="time"> Time
+                    <select className="select">
+                        <option value="value1">1m</option>
+                        <option value="value2">2m</option>
+                        <option value="value3">5m</option>
+                    </select>
+                </label>
+            </div>
         </div>
     )
 }
